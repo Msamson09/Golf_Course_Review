84436777f6fcee865325732116f363d655ccd83c
Msamson09 <michaelssamson@gmail.com>
2021-09-06 20:01:38 -0700

    adds signup function to views

--- main_app/views.py
+++ main_app/views.py
@@ -32,3 +32,22 @@ class GolfCourseUpdate(UpdateView):
     'price_18holes_wo_cart',
     'price_18holes_w_cart'
   ]
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    # This is how to create a 'user' form object
+    # that includes the data from the browser
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      # This will add the user to the database
+      user = form.save()
+      # This is how we log a user in
+      login(request, user)
+      return redirect('cats_index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  # A bad POST or a GET request, so render signup.html with an empty form
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'signup.html', context)
