7f93978af2843428c80c6973a1d846eaf11c7153
Msamson09 <michaelssamson@gmail.com>
2021-09-06 18:32:18 -0700

    stubbed up urls.py

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('', views.Home.as_view(), name='home'),
+    path('golfcourse/', views.golfcourse_index, name='golfcourse_index'),
+    path('golfcourse/<int:golfcourse_id>/', views.golfcourse_detail, name='golfcourse_detail'),
+    path('golfcourse/crate/', views.GolfCourseCreate.as_view(), name='golfcourse_create'),
+    path('golfcourse/<int:pk>/update/', views.GolfCourseUpdate.as_view(), name='golfcourse_update')
 ]
