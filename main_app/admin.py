6baf9cbc0170d79e56b94fc56898ed31c7ae0f09
Msamson09 <michaelssamson@gmail.com>
2021-09-09 07:33:02 -0700

    adds photo to admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import GolfCourse
+from .models import GolfCourse, Photo
 
 # Register your models here.
 admin.site.register(GolfCourse)
+admin.site.register(Photo)
 
